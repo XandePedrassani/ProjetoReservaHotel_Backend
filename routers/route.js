eeb5e9704b721e03c1a635935dc13f3401a01b82
XandePedrassani <alexandrecouss@hotmail.com>
2025-02-13 20:36:49 -0300

    Removendo lixos

--- routers/route.js
+++ routers/route.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const userController = require('../controllers/userController');
-const receitaController = require('../controllers/receitaController');
-const categoriaController = require('../controllers/categoriaController');
 const authController = require('../controllers/authController');
 
 const clienteController = require('../controllers/clienteController');
@@ -24,21 +22,6 @@ router.post('/usuarios', authenticateToken, userController.postUser);
 router.get('/usuarios/:id', authenticateToken, userController.getUsersById);
 router.put('/usuarios/:id', authenticateToken, userController.putUser);
 router.delete('/usuarios/:id', authenticateToken, userController.deleteUser);
-
-router.get('/receitas', authenticateToken, receitaController.getReceitas);
-router.post('/receitas', authenticateToken, receitaController.postReceita);
-router.get('/receitas/:id', authenticateToken, receitaController.getReceitaById);
-router.put('/receitas/:id', authenticateToken, receitaController.putReceita);
-router.delete('/receitas/:id', authenticateToken, receitaController.deleteReceita);
-
-router.get('/categorias', authenticateToken, categoriaController.getCategorias);
-router.post('/categorias', authenticateToken, categoriaController.postCategoria);
-router.get('/categorias/:id', authenticateToken, categoriaController.getCategoriaById);
-router.put('/categorias/:id', authenticateToken, categoriaController.putCategoria);
-router.delete('/categorias/:id', authenticateToken, categoriaController.deleteCategoria);
-
-router.get('/categorias/:id/receitas', authenticateToken, receitaController.getReceitasByCategoria);
-
 
 router.get('/clientes', authenticateToken, clienteController.getClientes);
 router.post('/clientes', authenticateToken, clienteController.postCliente);
